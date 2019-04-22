f491bcf6d4c3e785bcbcc8e113f6fd6722fe94f1
storm <storm.aref@gmail.com>
2019-04-22 21:24:50 +0430

    dbaccess

--- app/src/main/java/com/example/maskhare/DatabaseAccess.java
+++ app/src/main/java/com/example/maskhare/DatabaseAccess.java
@@ -6,19 +6,14 @@ import android.database.sqlite.SQLiteDatabase;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseAccess extends SQLiteAssetHelper {
-
-    private static String DATABASE_NAME = "mydb.db";
-    private static int DATABASE_VERSION = 1;
-
     SQLiteDatabase db;
 
-
     public DatabaseAccess(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        super(context, "mydb.db", null, 1);
         db = getWritableDatabase();
     }
 
-    public SQLiteDatabase getDb(){
+    public SQLiteDatabase getDb() {
         return db;
     }
 
